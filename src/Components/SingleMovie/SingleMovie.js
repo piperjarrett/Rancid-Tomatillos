534e8e9a4d82f34b761cc1c2ac607237e07c8af6
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-10-18 19:37:43 -0600

    removed the card component from single movie

--- src/Components/SingleMovie/SingleMovie.js
+++ src/Components/SingleMovie/SingleMovie.js
@@ -2,27 +2,32 @@ import React, { Component } from "react";
 import Card from "../Card/Card";
 import dayjs from "dayjs";
 import { render } from "@testing-library/react";
-import "./SingleMovie.css"
+import "./SingleMovie.css";
+import { Link } from "react-router-dom";
 
 const SingleMovie = ({ singleMovie, goHome }) => {
-  return !singleMovie ? <div className='spinner-container'>
-    <div className='loading-spinner'>
+  return !singleMovie ? (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
     </div>
-  </div> : (
+  ) : (
     <div>
-      <Card
-        title={singleMovie.title}
-        tagline={singleMovie.tagline}
-        backdrop_path={singleMovie.backdrop_path}
-        // video={video}
-        overview={singleMovie.overview}
-        average_rating={singleMovie.average_rating}
-        release_date={dayjs(singleMovie.release_date).format("MM/DD/YYYY")}
-        genres={singleMovie.genres}
-        runtime={singleMovie.runtime}
-        key={singleMovie.id}
-        goHome={goHome}
-      />
+      <div className="card" id={singleMovie.id}>
+        <h3>{singleMovie.title}</h3>
+        <p>{singleMovie.tagline}</p>
+        <img className="backdrop" src={singleMovie.backdrop_path} />
+
+        <p>{singleMovie.overview}</p>
+        <p>🍅 Rating: {singleMovie.average_rating.toFixed(2)}</p>
+        <p>Release Date: {singleMovie.release_date}</p>
+        <p>Genre: {`${singleMovie.genres}`}</p>
+        <p>Runtime: {singleMovie.runtime} Minutes</p>
+        <Link exact to="/" className="nav">
+          <button className="home-button" onClick={() => goHome()}>
+            Home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
